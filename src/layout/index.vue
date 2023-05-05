d8caba9d4a73e25b6ca539c97811451b62f19d71
liujin <2454243839@qq.com>
2023-05-05 16:06:34 +0800

    框架样式

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <router-view></router-view>
+  <div class="layout-box">
+    <div class="header-box">头部导航</div>
+    <router-view class="content-box"></router-view>
+  </div>
 </template>
 
 <script></script>
+
+<style scoped lang="scss">
+.layout-box {
+  height: 100%;
+  .header-box {
+    width: 100%;
+    height: 100px;
+    background-color: gray;
+  }
+  .content-box {
+    height: calc(100% - 100px);
+  }
+}
+</style>
